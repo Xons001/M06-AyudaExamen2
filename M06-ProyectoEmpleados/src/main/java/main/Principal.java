bed35f2bd530a10a75d7df216bc4c437eb93a82a
Sean Saez Fuller <sean.saez.f@gmail.com>
2020-01-15 19:11:33 +0100

    Todos los ejercicios acabados

--- M06-ProyectoEmpleados/src/main/java/main/Principal.java
+++ M06-ProyectoEmpleados/src/main/java/main/Principal.java
@@ -3,6 +3,7 @@ package main;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
@@ -18,6 +19,11 @@ public class Principal {
 		return numero;
 	}
 
+	public static float leerFloat() {
+		Scanner lector = new Scanner(System.in);
+		Float numero = lector.nextFloat();
+		return numero;
+	}
 	public static String leerString() {
 		Scanner lector = new Scanner(System.in);
 		String texto= lector.nextLine();
@@ -42,6 +48,12 @@ public class Principal {
 			System.out.println("--------------------------------------------------");
 			System.out.println("EJERCICIO 1");
 			insertDepartamento(conn);
+
+			System.out.println("EJERCICIO 2");
+			//storeProcedureUpdate();
+
+			System.out.println("EJERCICIO 3");
+			consultaEmpleadosXDepartamento();
 		} catch (Exception e) {
 			System.out.println("No se ha podido conectar a la base de datos");		
 		}
@@ -85,16 +97,53 @@ public class Principal {
 		} 
 	}
 
-	public static void storeProcedureUpdate(Float comision) {
+	public static void storeProcedureUpdate() {
 		CallableStatement cs;
 
 		try (Connection conn = DriverManager.getConnection(url, usuari, contr);
 				Statement stmt = conn.createStatement()) {
 
-			cs = (CallableStatement) conn.prepareCall("{ call tablasProcedures() }");
+			cs = (CallableStatement) conn.prepareCall("{ call updateComisionEmpleados(?) }");
+
+			System.out.println("Que comision quieres que tengan los vendedores");
+			float comisionDato = leerFloat();
+			cs.setFloat(1, comisionDato);
 			cs.executeUpdate();
 
 			System.out.println("Comision de los vendedores cambiada");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void consultaEmpleadosXDepartamento() {
+		CallableStatement cs;
+		ResultSet rs;
+
+		try (Connection conn = DriverManager.getConnection(url, usuari, contr);
+				Statement stmt = conn.createStatement()) {
+
+			cs = (CallableStatement) conn.prepareCall("{ call consultaEmpleadosDepartamento(?) }");
+
+			System.out.println("Que departamento quieres ver?");
+			String nomDep = leerString();
+			cs.setString(1, nomDep);
+			cs.executeUpdate();
+
+			rs = cs.executeQuery();
+			while(rs.next()) {
+				System.out.println("==================================");
+				System.out.println("ID-Emp"+"->"+rs.getInt(1));
+				System.out.println("Apellido"+"->"+rs.getString(2));
+				System.out.println("Oficio"+"->"+rs.getString(3));
+				System.out.println("Dir"+"->"+rs.getInt(4));
+				System.out.println("Fecha Alta"+"->"+rs.getDate(5));       
+				System.out.println("Salario"+"->"+rs.getFloat(6));
+				System.out.println("Comision"+"->"+rs.getFloat(7));
+				System.out.println("dept-no"+"->"+rs.getInt(8));   
+				System.out.println("==================================");
+			}
+
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
